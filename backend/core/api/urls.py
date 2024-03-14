27a3cf78e19d56136fbc8f7fcb1a55b6002e4e89
Igoreduardobraga <94845990+Igoreduardobraga@users.noreply.github.com>
2024-03-13 22:15:19 -0300

    adicionando backend e integração do backend com frontend

---
+++ backend/core/api/urls.py
@@ -0,0 +1,12 @@
+from rest_framework.routers import DefaultRouter
+from posts.api.urls import post_router
+from django.urls import path, include
+
+router = DefaultRouter()
+
+#posts
+router.registry.extend(post_router.registry)
+
+urlpatterns = [
+    path('', include(router.urls))
+]
--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,35 +1,54 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState, useEffect } from "react";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [data, setData] = useState([]);
+
+  useEffect(() => {
+    async function fetchData() {
+      console.log(import.meta.env.VITE_API_URL);
+      try {
+        const response = await fetch(`${import.meta.env.VITE_API_URL}`);
+        if (!response.ok) {
+          throw new Error("Network response was not ok");
+        }
+        const result = await response.json();
+        const postsResponse = await fetch(result.posts);
+        const posts = await postsResponse.json();
+        console.log(posts);
+
+        setData(posts);
+      } catch (error) {
+        console.error("Error fetching data:", error);
+      }
+    }
+
+    fetchData();
+  }, []);
 
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+      <div
+        style={{
+          display: "flex",
+          flexDirection: "column",
+          alignItems: "center",
+          justifyContent: "center",
+          height: "100vh",
+          width: "100%",
+          
+        }}
+      >
+        {data.map((item, index) => (
+          <div
+            key={index}
+            style={{marginTop: "0.5rem"}}
+          >
+            {item.title}
+          </div>
+        ))}
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
