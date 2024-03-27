783aa9404165be09788fb985eaffdeb227e75c81
Rubia-Souza <rubia.alice.m.s@gmail.com>
2024-03-27 10:38:05 -0300

    feat(forms): Cria formulário basico para cadastro de remédios

--- main_app/forms.py
+++ main_app/forms.py
@@ -5,6 +5,7 @@ from django.core.exceptions import ValidationError
 from django.contrib.auth.forms import AuthenticationForm
 
 from .models import User
+from .models import Medication
 import re
 
 class CustomUserCreationForm(UserCreationForm):
@@ -31,3 +32,53 @@ class CustomAuthenticationForm(AuthenticationForm):
         'invalid_login': ("A senha ou usuário está incorreto. Por favor verifique essas informações e tente novamente."),
         'inactive': ("Usuário Inativo."),
     }
+
+class CustomMedicationCreationForm(forms.Form):
+    name_field = forms.CharField(
+        label="Nome do medicamento", 
+        max_length=100, 
+        required=True, 
+        help_text="Nome do medicamento que será utilizado no tratamento."
+    )
+
+    frequency_field = forms.IntegerField(
+        label="Frequência (horas)", 
+        min_value=1, 
+        required=True, 
+        help_text="Quantidade de horas entre as dosagens."
+    )
+    
+    dose_field = forms.CharField(
+        label="Dosagem", 
+        max_length=100, 
+        required=True, 
+        help_text="Quanto do medicamento é administrado. Por exemplo: 1 comprimido, 10 mg, etc..."
+    )
+
+    start_datetime_field = forms.SplitDateTimeField(
+        label="Data de início", 
+        required=True, 
+        help_text="Data em que irá iniciar o tratamento com o remédio."
+    )
+
+    end_datetime_field = forms.SplitDateTimeField(
+        label="Data de fim", 
+        required=False,
+        help_text="Última data que o remédio precisa ser aministrado."
+    )
+
+    notify_field = forms.ChoiceField(
+        label="Receber avisos", 
+        required=True, 
+        help_text="Deseja receber lembretes de tomar o remédio pelo telefone?"
+    )
+
+    observations_field = forms.CharField(
+        label="Observações", 
+        required=False, 
+        help_text="Informações extras sobre o acompanhamento"
+    )
+
+    class Meta():
+        model = Medication
+        fields = ('name', 'frequency', 'dose', 'start_datetime', 'end_date', 'observations',)
